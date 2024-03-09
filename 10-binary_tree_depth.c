cf430a8534fa1f29cbf21b322ce411f590dfb985
emmmamuli <kilokoemma175@gmail.com>
2024-03-09 10:01:31 -0800

    depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,15 +9,5 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
-	size_t depth = 0;
-
-	while (tree->parent != NULL)
-	{
-		depth++;
-		tree = tree->parent;
-	}
-	return (depth);
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
